bbe0d02b07b55e25b514cd61444728ecaf19b3d0
talasov <talasov2017@yandex.ru>
2023-02-28 15:10:45 +0300

    commit7

--- yatube_api/api/permission.py
+++ yatube_api/api/permission.py
@@ -8,13 +8,7 @@ class IsOwnerOrReadOnly(BasePermission):
 
 class IsAuthorOrReadOnly(BasePermission):
     def has_permission(self, request, view):
-        return (
-                request.method in SAFE_METHODS
-                or request.user.is_authenticated
-        )
+        return request.method in SAFE_METHODS or request.user.is_authenticated
 
     def has_object_permission(self, request, view, obj):
-        return (
-                request.method in SAFE_METHODS
-                or obj.author == request.user
-        )
+        return request.method in SAFE_METHODS or obj.author == request.user
