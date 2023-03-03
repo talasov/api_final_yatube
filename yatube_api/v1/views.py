9596afe979be93fd62e4a70154981d47e63e3204
talasov <talasov2017@yandex.ru>
2023-03-03 10:52:36 +0300

    update v1/views, v1/permissions

--- yatube_api/v1/views.py
+++ yatube_api/v1/views.py
@@ -1,7 +1,9 @@
 from django.shortcuts import get_object_or_404
-from rest_framework.permissions import IsAuthenticated, IsAuthenticatedOrReadOnly
+from rest_framework.permissions import (
+    IsAuthenticated, IsAuthenticatedOrReadOnly
+)
 from rest_framework.pagination import LimitOffsetPagination
-from rest_framework import filters, status, viewsets
+from rest_framework import filters, viewsets
 
 from posts.models import Follow, Group, Post, User
 from .permissions import IsAuthorOrReadOnly
